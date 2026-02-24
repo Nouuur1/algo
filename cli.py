382e0f18962983a22f67cc1d2ab39850040e6ac6
wiemhamzaoui <wiemhamzaoui4@gmail.com>
2026-02-24 22:09:30 +0100

    fixed CLI error

--- cli.py
+++ cli.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-"""Interactive Command-Line Interface for Maghara Sarf
+Interactive Command-Line Interface for Maghara Sarf
 Allows users to manage roots, schemes, and generate/validate derivatives
 """
 
